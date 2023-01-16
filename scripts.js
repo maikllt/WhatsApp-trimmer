86366489b7ff0898c62fdf750b933eabe003fe75
Tomas <tomas@Tomas-MacBook.local>
2023-01-17 00:14:39 +0800

    initial commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+function getPhoneNumber() {
+    let phone = document.getElementById("WAphoneNumber").value;
+    //remove unsupported by WA API characters from phone number
+    phone = phone.replace(/[ \-\+()]/g, "");
+    return phone;
+}
+function getText() {
+    let text = document.getElementById("WAtext").value;
+    text = text.replace(/\n/g, "%0A");
+    return !!text ? ("?text=" + text) : "";
+
+}
+function openWA() {
+    let url = "https://wa.me/" + getPhoneNumber() + getText();
+    window.open(url, "_blank");
+    document.getElementById("WAphoneNumber").value = "";
+}
+function clearWAtext() {
+    document.getElementById("WAtext").value = "";
+    return;
+}
